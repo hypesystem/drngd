4a56b26abb69d31ebe7310a28fff848bc0e26feb
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-12-16 12:24:52 +0100

    mysql_error for v.php

--- v.php
+++ v.php
@@ -2,7 +2,7 @@
 if(isset($_GET['v']) && intval($_GET['v'],36) > 0) {
     $id = intval($_GET['v'],36);
     include('mysql_connect.php');
-    $url = mysql_query("SELECT * FROM link WHERE id='$_GET[v]'");
+    $url = mysql_query("SELECT * FROM link WHERE id='$_GET[v]'") or die(mysql_error());
     $url = mysql_fetch_assoc($url);
     mysql_query("INSERT INTO link_visit (link_id,user_agent,ip,timestamp) VALUES ('".$id."','".addslashes($_SERVER['HTTP_USER_AGENT'])."','".addslashes($_SERVER['REMOTE_ADDR'])."','".date("Y-m-d H:i:s")."')") or die(mysql_error());
     if(!isset($url['href']) || $url['href'] == "") $url['href'] = "index.php";
