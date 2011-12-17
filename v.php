f94cb58320e7abe70ddf0a3d0b5422f58b856d9e
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-12-17 12:26:06 +0100

    testing redirection with rewrite 7

--- v.php
+++ v.php
@@ -2,6 +2,7 @@
 if(isset($_GET['v']) && intval($_GET['v'],36) > 0) {
     $id = intval($_GET['v'],36);
     echo $id;
+    echo ' '.base_convert($id,36);
     include('mysql_connect.php');
     mysql_query("INSERT INTO link_visit (link_id,user_agent,ip,timestamp) VALUES ('".$id."','".addslashes($_SERVER['HTTP_USER_AGENT'])."','".addslashes($_SERVER['REMOTE_ADDR'])."','".date("Y-m-d H:i:s")."')") or die(mysql_error());
     $url = mysql_query("SELECT * FROM link WHERE id='".intval($_GET[v],36)."'") or die(mysql_error());
