b22c8402d458314d5d022815cd5256901c75f8f8
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-12-20 18:30:55 +0100

    Fixed error with www. links

--- create-link.php
+++ create-link.php
@@ -17,7 +17,7 @@ else {
     if(isset($match)) {
         include('mysql_connect.php');
         mysql_query("INSERT INTO link (href,timestamp) VALUES ('".addslashes($match)."','".date("Y-m-d H:i:s")."')") or die(mysql_error());
-        $get = mysql_query("SELECT * FROM link WHERE href='".addslashes($_GET['url'])."' ORDER BY id DESC") or die(mysql_error());
+        $get = mysql_query("SELECT * FROM link WHERE href='".addslashes($match)."' ORDER BY id DESC") or die(mysql_error());
         $get = mysql_fetch_assoc($get);
         echo '<span class="green">Success!</span> Your short link is: <a href="http://drng.dk/'.base_convert($get['id'],10,36).'" target="_blank">drng.dk/'.base_convert($get['id'],10,36).'</a><br />';
         echo 'To track and view statistics for your link, see <a href="http://drng.dk/stats.php?l='.base_convert($get['id'],10,36).'">http://drng.dk/stats.php?l='.base_convert($get['id'],10,36).'</a>';
