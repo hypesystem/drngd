08481bb5e8cba2ba55e9bfd51c3dbc795595a69b
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-01-20 22:22:54 +0100

    Updated verifyLinkKey function

--- func.php
+++ func.php
@@ -5,8 +5,10 @@ function verifyLinkKey($key) {
     include("mysql_connect.php");
     $key = intval(trim(strtolower($key)), 36);
     $get = mysql_query("SELECT * FROM link WHERE id='{$key}' LIMIT 1") or die(mysql_error());
-    $get = mysql_fetch_assoc($get) or die(mysql_error());
-    if(isset($get['href'])) return true;
+    if(mysql_num_rows($get) != false && mysql_num_rows($get) > 0) {
+        $get = mysql_fetch_assoc($get) or die(mysql_error());
+        if(isset($get['href'])) return true;
+    }
     return false;
 }
 
