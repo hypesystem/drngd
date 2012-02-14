83a1fddc9648a96590c374e284f4e4c9f78d5d59
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-02-14 23:31:27 +0100

    Highchart.js script added to stats, removed "stop censorship" logo

--- stats.php
+++ stats.php
@@ -94,13 +94,66 @@ arsort($user_browser);
         <meta charset="utf-8" />
         <base href="http://drng.dk/" />
         <title>drng</title>
-        <link rel="stylesheet" type="text/css" href="http://drng.dk/style/drngd.css" />
-        <script type="text/javascript" src="http://drng.dk/script/jquery.js"></script>
-        <script type="text/javascript" src="http://drng.dk/script/script.js"></script>
+        <link rel="stylesheet" type="text/css" href="style/drngd.css" />
+        <script type="text/javascript" src="script/jquery.js"></script>
+        <script type="text/javascript" src="script/script.js"></script>
+        <script type="text/javascript" src="script/highcharts.js"></script>
+        <script type="text/javascript">
+            var chart;
+            $(document).ready(function() {
+                chart = new Highcharts.Chart({
+                    chart: {
+                        renderTo: 'container',
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false
+                    },
+                    title: {
+                        text: 'Visitors\' Operating Systems'
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                        }
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                color: '#000000',
+                                connectorColor: '#000000',
+                                formatter: function() {
+                                    return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                                }
+                            }
+                        }
+                    },
+                    series: [{
+                        type: 'pie',
+                        name: 'Browser share',
+                        data: [
+                            ['Firefox',   45.0],
+                            ['IE',       26.8],
+                            {
+                                name: 'Chrome',    
+                                y: 12.8,
+                                sliced: true,
+                                selected: true
+                            },
+                            ['Safari',    8.5],
+                            ['Opera',     6.2],
+                            ['Others',   0.7]
+                        ]
+                    }]
+                });
+            });
+        </script>
     </head>
     <body>
         <div id="stats-head">
-            <a href="http://drng.dk"><img src="http://drng.dk/style/logo.png" alt="deranged" title="deranged" /></a>
+            <a href="http://drng.dk"><img src="style/logo.png" alt="deranged" title="deranged" /></a>
             <p>Statistics for <a href="http://drng.dk/<?php echo $_GET['l']; ?>" target="_blank">drng.dk/<?php echo $_GET['l']; ?></a></p>
         </div>
         <br />
@@ -209,6 +262,9 @@ arsort($user_browser);
                                     </tr>
                                 </tbody>
                             </table>
+                        </td>
+                        <td style="border: 1px solid white;" colspan="2">
+                            <div id="container" style="height: 200px; margin: 0 auto;"></div>
                         </td>
                     </tr>
                 </tbody>
