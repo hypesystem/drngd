8726ff6bf5dc1e5801a23cd185d7088d0607c5cc
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-02-22 11:53:30 +0100

    Reorganizing project

---
+++ api/create-link.php
@@ -0,0 +1,27 @@
+<?php
+if(!isset($_GET['url']) || $_GET['url'] == "" || $_GET['url'] == "http://url") {
+    echo json_encode(array('success' => false, 'error' => 'No url supplied'));
+}
+else {
+    $match;
+    if(preg_match('#^ftp\:\/\/[aA-zZ0-9.-]+\.[aA-zZ]+#',$_GET['url']) ||
+            preg_match('#^http\:\/\/[aA-zZ0-9.-]+\.[aA-zZ]+#',$_GET['url']) ||
+            preg_match('#^https\:\/\/[aA-zZ0-9.-]+\.[aA-zZ]+#',$_GET['url'])) {
+        $match = $_GET['url'];
+    }
+    else if(preg_match("#^www\.[aA-zZ0-9.-]+\.[aA-zZ]+#",$_GET['url'])) {
+        $match = "http://".$_GET['url'];
+    }
+    else {
+        echo json_encode(array('success' => false, 'error' => 'Url supplied is invalid'));
+    }
+    
+    if(isset($match)) {
+        include('mysql_connect.php');
+        mysql_query("INSERT INTO link (href,timestamp) VALUES ('".addslashes($match)."','".date("Y-m-d H:i:s")."')") or die(mysql_error());
+        $get = mysql_query("SELECT * FROM link WHERE href='".addslashes($match)."' ORDER BY id DESC") or die(mysql_error());
+        $get = mysql_fetch_assoc($get);
+        echo json_encode(array('success' => true, 'link' => array('id' => base_convert($get['id'],10,36),'link' => 'http://drng.dk/'.base_convert($get['id'],10,36))));
+    }
+}
+?>
---
+++ sys/func.php
@@ -0,0 +1,20 @@
+<?php
+
+function verifyLinkKey($key) {
+    if(!isset($key) || $key == "" || !preg_match("/^[0-9a-z]+/",$key)) return false;
+    include("mysql_connect.php");
+    $key = intval(trim(strtolower($key)), 36);
+    $get = mysql_query("SELECT * FROM link WHERE id='{$key}' LIMIT 1") or die(mysql_error());
+    if(mysql_num_rows($get) != false && mysql_num_rows($get) > 0) {
+        $get = mysql_fetch_assoc($get) or die(mysql_error());
+        if(isset($get['href'])) return true;
+    }
+    return false;
+}
+
+/**
+ * Bash script to save commit # hashtag to version.log:
+ * git describe --all --long | tr "-" " " | awk '{ print $3 }' > version.log
+ */
+
+?>
