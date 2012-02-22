8ac1fe6c5a7be19f63a8b93ad9e4577877e233fb
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-02-22 12:24:55 +0100

    Reorganizing project

--- sys/create.php
+++ sys/create.php
@@ -1,16 +1,3 @@
-<?php
-include("func.php");
-if(verifyLinkKey($_GET['v'])) {
-    $id = intval($_GET['v'],36);
-    include('mysql_connect.php');
-    mysql_query("INSERT INTO link_visit (link_id,user_agent,ip,timestamp) VALUES ('".$id."','".addslashes($_SERVER['HTTP_USER_AGENT'])."','".addslashes($_SERVER['REMOTE_ADDR'])."','".date("Y-m-d H:i:s")."')") or die(mysql_error());
-    $url = mysql_query("SELECT * FROM link WHERE id='".intval($_GET[v],36)."'") or die(mysql_error());
-    $url = mysql_fetch_assoc($url) or die(mysql_error());
-    if(!isset($url['href']) || $url['href'] == "") $url['href'] = "index.php";
-    header('Location: '.$url['href']);
-}
-else {
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -39,4 +26,3 @@ else {
     <div class="version-box">version 1.1.0.<?php include("version.log"); ?></div>
 </body>
 </html>
-<?php } ?>
