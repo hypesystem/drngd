ffe524f106ce8fee1cf500cacefdcc317f7ead7f
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-02-28 14:49:06 +0100

    Changed page title

--- sys/create.php
+++ sys/create.php
@@ -1,5 +1,5 @@
 <?php
     $scripts = array("lib/jquery.js","lib/zeroclipboard/ZeroClipboard.js","script/create.js");
     $page_content = '<input type="text" name="url" value="http://url" class="inactive" id="url-input" /><input type="submit" value="Shorten link" id="url-submit" /><div id="output"></div>';
-    $page_title = 'Shorten links';
+    $page_title = 'Shorten link';
 ?>
