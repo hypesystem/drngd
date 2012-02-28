48cc1a6b99987e7b21e42a234ce7bcdc1033a581
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-02-28 14:52:29 +0100

    Updated layout to 1.2.1

--- style/layout.php
+++ style/layout.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8" />
         <!--<base href="http://drng.dk/" />-->
         <title><?php echo $page_title; ?> | drng.dk</title>
-        <link rel="stylesheet" <?php /*media="screen and (min-width: 701px)"*/ ?> type="text/css" href="style/drngd.css" />
-        <?php /*<link rel="stylesheet" media="only screen and (max-width: 700px)" type="text/css" href="style/mobile.css" />*/ ?>
+        <link rel="stylesheet" media="screen and (min-width: 701px)" type="text/css" href="style/drngd.css" />
+        <link rel="stylesheet" media="only screen and (max-width: 700px)" type="text/css" href="style/mobile.css" />
         <?php foreach($stylesheets as $s): ?>
             <link rel="stylesheet" type="text/css" href="<?php echo $s; ?>"></script>
         <?php endforeach; ?>
@@ -18,10 +18,12 @@
             <div id="bar-content">
                 <nav id="bar-menu">
                     <a href=".">+New</a>
+                    <a href="!doc/why-use-it">About</a>
+                    <a href="!doc/api-documentation">API</a>
                 </nav>
                 <div id="version">
                     <a href="http://github.com/hypesystem/drngd" target="_blank">
-                        1.2.0.<?php if(file_exists("sys/version.log")) include("sys/version.log"); else echo "null"; ?>
+                        1.2.1.<?php if(file_exists("sys/version.log")) include("sys/version.log"); else echo "null"; ?>
                     </a>
                 </div>
             </div>
