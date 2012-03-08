0bb8101911f32657f76164318de4b9b4be7782fc
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-03-08 16:50:21 +0100

    Only uses normal stylesheet if not forced pc version and device width exceeds 600px

--- style/layout.php
+++ style/layout.php
@@ -5,7 +5,7 @@
         <link rel="shortcut icon" type="image/png" href="style/imgs/fav.png" />
         <base href="http://drng.dk/" />
         <title><?php echo $page_title; ?> | drng.dk</title>
-        <link rel="stylesheet" <?php if(!isset($_COOKIE['force-pc']) || !$_COOKIE['force-pc']) echo 'media="Screen"'; ?> type="text/css" href="style/drngd.css" />
+        <link rel="stylesheet" <?php if(!isset($_COOKIE['force-pc']) || !$_COOKIE['force-pc']) echo 'media="Screen and (min-device-width: 600px)"'; ?> type="text/css" href="style/drngd.css" />
         <?php if(!isset($_COOKIE['force-pc']) || !$_COOKIE['force-pc']) echo '<link rel="stylesheet" media="handheld, only screen and (max-device-width: 600px)" type="text/css" href="style/mobile.css" />'; ?>
         <?php foreach($stylesheets as $s): ?>
             <link rel="stylesheet" type="text/css" href="<?php echo $s; ?>"></script>
