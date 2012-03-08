e679fc7673008fb0ac3396a4fc47ee24652f7cd7
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-03-08 11:36:38 +0100

    Developing version 1.2.3

--- style/layout.php
+++ style/layout.php
@@ -24,7 +24,7 @@
                 </nav>
                 <div id="version">
                     <a href="http://github.com/hypesystem/drngd" target="_blank">
-                        1.2.2.<?php if(file_exists("sys/version.log")) include("sys/version.log"); else echo "null"; ?>
+                        1.2.3.<?php if(file_exists("sys/version.log")) include("sys/version.log"); else echo "null"; ?>
                     </a>
                 </div>
             </div>
