cdcb673c4c3bf20c0437fc2c439e5ff3623c4d63
Niels Abildgaard <niels.abildgaard@gmail.com>
2012-03-15 09:28:10 +0100

    Setup form for posting url to work with nojs

--- sys/create.php
+++ sys/create.php
@@ -1,5 +1,9 @@
 <?php
     $scripts = array("lib/jquery.js","lib/zeroclipboard/ZeroClipboard.js","script/create.js");
-    $page_content = '<input type="text" name="url" value="http://url" class="inactive" id="url-input" /><input type="submit" value="Shorten link" id="url-submit" /><div id="output"></div>';
+    $page_content = '   <form method="post" action=".">
+                            <input type="text" name="url" value="http://url" class="inactive" id="url-input" />
+                            <input type="submit" value="Shorten link" id="url-submit" />
+                        </form>
+                        <div id="output"></div>';
     $page_title = 'Shorten link';
 ?>
